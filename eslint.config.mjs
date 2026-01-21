84d290c9e6128e27f025b9772dde64f91bb004c0
Toheedullah-K3 <toheedullah002@gmail.com>
2026-01-21 09:28:21 +0500

    Eslint add

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,3 +14,27 @@
 // ];
 
 // export default eslintConfig;
+
+// .eslintrc.mjs
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+
+  {
+    // Override rules
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn', // <-- WARN instead of ERROR
+      'import/no-anonymous-default-export': 'off', // optional
+    },
+  },
+];
