8d893281987b788275f9b75b97c91f507c8492ab
Toheedullah-K3 <toheedullah002@gmail.com>
2026-04-01 16:36:11 +0500

    ThemeToggle improved

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,6 +1,5 @@
 import { navbarConfig } from '@/config/Navbar';
 import { Link } from 'next-view-transitions';
-import Image from 'next/image';
 import React from 'react';
 
 import Container from './Container';
